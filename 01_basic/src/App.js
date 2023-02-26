4638e8d7995e757b45f9fce3a3b50603085d195d
yoonjoowon <wndnjs6224@naver.com>
2023-02-26 13:50:41 +0900

    study

--- 01_basic/src/App.js
+++ 01_basic/src/App.js
@@ -1,21 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 // import Mycomponent from './MyComponent';
-import React from 'react';
+import React, {useState, useRef} from 'react';
 // import Hello from './Hello';
-import UseRef from './UseRef';
+import UserList from './UserList';
+import ArrayAdd2 from './ArrayAdd2';
 // 앞에는 함수명 뒤에는 .js가 생략된 파일명
 
- 
-
 function App(){
+
+  const [inputs, setInputs] = useState({username: "", email:"" });
+
+  const {username, email} = inputs;
+
+  const onChange = (e) => {
+    const {name, value} = e.target; 
+    // e.target은 onchange이벤트가 설정된input 태그를 가리킴
+    setInputs({
+      ...inputs,
+      [name] : value
+    })
+  }
+
+  const [users, setUsers]= useState([
+    {id:1, username: 'user1', email: 'user1@gmail.com'},
+    {id:2, username: 'user2', email: 'user2@gmail.com'},
+    {id:3, username: 'user3', email: 'user3@gmail.com'}
+]);
+
+  const nextId = useRef(4)
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+
+    setUsers(users.concat(user))
+    setInputs({
+      username:"",
+      email:""
+    });
+    nextId.current +=1;
+  }
+
+  const onRemove = id => {
+    setUsers(users.filter(user = user.id !== id));
+  }
+
   return(
-    <UseRef/>
+    <div>
+      <ArrayAdd2
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}        
+        />
+      <UserList users={users} onRemove={onRemove}/>
+    </div>
   );
 }
 
 export default App;
-
 
 
 // 5. function App(){
@@ -33,6 +80,7 @@ export default App;
 
 
 // 3. props : 두 컴포넌트 사이에 데이터 전송
+// 상위 컴포넌트가 하위 컴포넌트에 값을 전달할 때 사용
 
 // function App(){
 //   return (<Hello name='react' color='red'/>)
